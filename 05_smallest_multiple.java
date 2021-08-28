7affbc9adcb378289887840255480264cb1dc7df
Akshai <72917775+Akshai-2909@users.noreply.github.com>
2021-08-28 10:03:02 +0530

    Update 05_smallest_multiple.java

--- 05_smallest_multiple.java
+++ 05_smallest_multiple.java
@@ -1,12 +1,12 @@
 class main{
 	public static void main(String[] args){
 		       int a = 1;
-		first: while(a!=0){
+		 while(a!=0){
 			        for(int i = 1;i<=20;i++){
 				          if(a%i==0){
 					            if(i==20){
 					              System.out.println(a);
-					              break first;
+					              System.exit(1);
 					          }
 					            continue;
 				          }
